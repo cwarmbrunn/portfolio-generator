bb0fed695e525c8cef4f2fb102719876cf165d79
cwarmbrunn <cwarmbrunn97@gmail.com>
2022-03-24 15:39:14 -0400

    add node.js to generate html file

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require("fs");
+const generatePage = require("./src/page-template.js");
 
-const printProfileData = (profileDataArr) => {
-  // This ...
-  for (let i = 0; i < profileDataArr.length; i++) {
-    console.log(profileDataArr[i]);
-  }
-  console.log("=============");
+const profileDataArgs = process.argv.slice(2);
 
-  // Is the same as this...
-  profileDataArr.forEach((profileItem) => {
-    console.log(profileItem);
-  });
-};
+const [name, github] = profileDataArgs;
 
-printProfileData(profileDataArgs);
+fs.writeFile("./index.html", generatePage(name, github), (err) => {
+  if (err) throw err;
+
+  console.log("Portfolio complete - check out index.html to see the output!");
+});
